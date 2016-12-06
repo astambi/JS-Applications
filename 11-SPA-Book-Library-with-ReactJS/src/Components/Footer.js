f810c1beaed9f3805aaf4e9a1ee34d9adfdafd1c
Anna <annsta@gmail.com>
2016-12-06 19:28:15 +0200

    Book Library SPA with ReactJS

--- 11-SPA-Book-Library-with-ReactJS/src/Components/Footer.js
+++ 11-SPA-Book-Library-with-ReactJS/src/Components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="footer">
-                (c) React Book Library App
+                (c) Book Library SPA with ReactJS
             </div>
         );
     }
